c185561842a994b3b730e4b766a952533bf923e3
Rostyslav Lisovyi <rokokos97@gmail.com>
2022-09-16 22:17:15 +0200

    6_2 complete

---
+++ js/simulator_Javascript_3.js
@@ -0,0 +1,11 @@
+for (let i=1; i<=5; i+=2){
+    console.log(i);
+}
+for (let i=6; i<=10; i+=2){
+    console.log(i);
+}
+
+let sumUserNumbers = []
+for (let i=0; i<+infinity; i++){
+    userNumber
+}
